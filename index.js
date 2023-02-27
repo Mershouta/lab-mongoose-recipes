763c43bf10eedb2951a779fd5171001e02a6ee30
Mershouta <jchristkay@gmail.com>
2023-02-27 21:11:24 +0100

    Error i don't know how to fix

--- index.js
+++ index.js
@@ -21,3 +21,39 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+Recipe.create({
+  title: 'Pizza Margherita',
+  level: 'Easy Peasy',
+  ingredients: ['flour', 'water', 'yeast', 'tomatoes', 'mozzarella', 'basil'],
+  cuisine: 'Italian',
+  dishType: 'main_course',
+  duration: 60,
+  creator: 'Chef Luigi'
+}).then(recipe => {
+  console.log(`Created recipe: ${recipe.title}`);
+}).catch(error => {
+  console.error('Error creating recipe:', error);
+});
+
+const recipes = require('./data.json');
+
+Recipe.insertMany(recipes).then(recipes => {
+  recipes.forEach(recipe => {
+    console.log(`Inserted recipe: ${recipe.title}`);
+  });
+}).catch(error => {
+  console.error('Error inserting recipes:', error);
+});
+
+Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true }).then(recipe => {
+  console.log(`Updated recipe: ${recipe.title}`);
+}).catch(error => {
+  console.error('Error updating recipe:', error);
+});
+
+Recipe.deleteOne({ title: 'Carrot Cake' }).then(() => {
+  console.log('Deleted recipe: Carrot Cake');
+}).catch(error => {
+  console.error('Error deleting recipe:', error);
+});
